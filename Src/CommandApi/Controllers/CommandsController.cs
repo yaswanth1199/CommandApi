7864983cb610ed6e61972ff0786202f94e4c9ca8
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2022-02-11 23:30:36 +0530

    Initial Commit

---
+++ Src/CommandApi/Controllers/CommandsController.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+namespace CommandAPI.Controllers
+{
+[Route("api/[controller]")]
+ [ApiController]
+public class CommandsController : ControllerBase
+ {
+      public CommandsController()
+        {
+          
+        }
+ [HttpGet]
+ public ActionResult<IEnumerable<string>> Get()
+ {
+ return new string[] {"this", "is", "hard", "coded"};
+ }
+ }
+}
